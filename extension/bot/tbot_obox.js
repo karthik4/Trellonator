e900d51778570f0971d287520c48729ce8ca3768
Karthik K <karthik.k.nitk@gmail.com>
2014-12-24 00:01:50 -0800

    Basic tokenization framework

--- extension/bot/tbot_obox.js
+++ extension/bot/tbot_obox.js
@@ -1,8 +1,43 @@
 // The omnibox javascript
 
+function tokenizeCommand( command ) {
+    // Split on spaces
+    var tokenArray = command.split( " " );
+    var args = new Array();
+    var opts = new Array();
+
+    // The first token is the command
+
+    var com = tokenArray[ 0 ];
+
+    // The rest are either arguments or options
+    for (var i = tokenArray.length - 1; i > 0; i--) {
+        if( tokenArray[ i ].indexOf("-") == 0 ) {
+            opts.push( tokenArray[ i ] );
+        } else {
+            args.push( tokenArray[ i ] );
+        }
+    };
+}
+
+function commandParser( command ) {
+    // The command parser function
+
+    tokens = tokenizeCommand( command );
+    
+    if( tokens == null ) {
+        throw "is Empty";
+    }
+
+
+}
+
 chrome.omnibox.onInputEntered.addListener( 
     function( text ) {
         console.log( 'TrelloBot: ' + text );
-        alert( "Keyword fired!" );
+        
+        // We got a command for the TrelloBot, now parse the command
+
+
     }
 );
